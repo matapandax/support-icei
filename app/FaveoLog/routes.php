5cda66fdc322d79885e553c23738c305312c0491
root <root@ubuntu-cloudpanel.localhost>
2024-01-10 03:50:25 +0000

    V-01

---
+++ app/FaveoLog/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Breadcrumbs::register('logs', function ($breadcrumbs) {
+    $breadcrumbs->parent('setting');
+    $breadcrumbs->push('System Logs', route('logs'));
+});
+Route::middleware('web', 'auth', 'roles')->group(function () {
+    Route::get('logs', [\App\FaveoLog\controllers\LogViewerController::class, 'index'])->name('logs');
+});
